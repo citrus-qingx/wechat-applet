55c25abffe76a8757a2a171e1cde5d9fab3f20bb
citrus <1119309385@qq.com>
2021-12-03 13:14:24 +0800

    lottery page xtml

--- wechat-applet/miniprogram/pages/lottery/lottery.js
+++ wechat-applet/miniprogram/pages/lottery/lottery.js
@@ -5,7 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
+    list:[
+      {
+        id:0,
+        name:"丁香",
+        value:"丁香"
+      },
+      {
+        id:1,
+        name:"海棠",
+        value:"海棠"
+      },
+      {
+        id:2,
+        name:"竹园",
+        value:"竹园"
+      },
+      {
+        id:3,
+        name:"综合楼",
+        value:"综合楼"
+      }
+    ],
+    checkedList:[],
+    backgroundColor: "#92a4b0",
+    fontColor: "white"
+  },
 
+  /**
+   * button点击事件监听
+   */
+  clickButton: function(e) {
+},
+
+  /*复选框选中事件*/
+  HandelItemChange(e){
+    // 1 获取被选中的复选框的值
+    const checkedList = e.detail.value;
   },
 
   /**
